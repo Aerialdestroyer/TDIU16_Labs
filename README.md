47e238a8dbc6b6bed00cf7df9071e48636cb5416
Aerialdestroyer <bugatti430@gmail.com>
2021-03-30 17:11:38 +0200

    initial commit, lab1 probably done

---
+++ README.md
@@ -0,0 +1,7 @@
+TDIU16 Labs
+======
+**TDIU16 Labs** Made for a university course.
+
+## Usage
+
+### Command line arguments
---
+++ lab1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    int tot_len = 0;
+    for(int i; i<argc; i++){
+        tot_len += strlen(argv[i]);
+        printf("%-17s%4lu\n",argv[i],strlen(argv[i]));
+    }
+    float avrg = (float)tot_len/argc;
+    printf("\n%-17s%4u\n", "Total length", tot_len);
+    printf("%-17s%4.2f\n", "Average length", avrg);
+
+    return tot_len;
+}
